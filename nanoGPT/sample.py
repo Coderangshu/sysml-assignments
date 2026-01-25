ee259903f06145be9888413a2ad6da43dc261dac
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2026-01-25 20:13:15 +0530

    use_cache added as global variable for changing via cli

--- nanoGPT/sample.py
+++ nanoGPT/sample.py
@@ -14,7 +14,7 @@ import time
 init_from = 'gpt2-xl' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
 # start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-start = "Django is framework in python \n"
+start = "Django is framework in "
 # num_samples = 10 # number of samples to draw
 num_samples = 1
 max_new_tokens = 500 # number of tokens generated in each sample
@@ -26,6 +26,7 @@ device = 'mps' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 # dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 dtype = 'float32'
 compile = False # use PyTorch 2.0 to compile the model to be faster
+use_cache = True # whether to use the past key/values cache during generation
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
@@ -91,7 +92,7 @@ with torch.no_grad():
     start = time.time()
     with ctx:
         for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k, use_cache=True)
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k, use_cache=use_cache)
             print(decode(y[0].tolist()))
             print('---------------')
     end = time.time()
